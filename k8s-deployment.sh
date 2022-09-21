ef158f4e31b400c8d46eac95373035ec4ea75b02
Sudarshan Reddy <sudarshang.reddy@gmail.com>
2022-09-22 00:55:50 +0530

    kub

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i 's#replace#iamharryindoc/numeric-app:${GIT_COMMIT}#g' k8s_deployment_service.yaml
+sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 #kubectl -n default get deployment ${deploymentName} > /dev/null
 kubectl apply -f k8s_deployment_service.yaml
