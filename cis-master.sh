2412a7ef9e2a1de800b05599f9784876ac87fb60
Sudarshan Reddy <sudarshang.reddy@gmail.com>
2022-09-23 18:22:30 +0530

    if statement

--- cis-master.sh
+++ cis-master.sh
@@ -1,6 +1,6 @@
 total_fail=$(kube-bench run --targets node --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
 
-if [[ "$total_fail" -ne 0]]
+if [[ $total_fail -ne 0]]
 then
 	echo "CIS Benchmark Failed Master while testing for 1.2.7,1.2.8,1.2.9"
 	exit 1;
