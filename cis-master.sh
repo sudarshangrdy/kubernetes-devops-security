be0fb0579c5f95b35d2d4518d05685a09af73734
Sudarshan Reddy <sudarshang.reddy@gmail.com>
2022-09-23 18:16:56 +0530

    modified scripts

--- cis-master.sh
+++ cis-master.sh
@@ -1,4 +1,4 @@
-total_fail=$(kube-bench run --targets node --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+total_fail=$(kube-bench run --targets node --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
 
 if [[ "$total_fail" -ne 0]]
 then
