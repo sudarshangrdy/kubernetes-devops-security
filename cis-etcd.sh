1af8957312b05d3a8d967736a1afd0d614174868
Sudarshan Reddy <sudarshang.reddy@gmail.com>
2022-09-23 18:27:10 +0530

    added

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -1,6 +1,6 @@
 total_fail=$(kube-bench run --targets node --version 1.15 --check 2.2 --json | jq .Totals.total_fail)
 
-if [[ $total_fail -ne 0]]
+if [[ $total_fail -ne 0 ]]
 then
 	echo "CIS Benchmark Failed Etcd while testing for 2.2"
 	exit 1;
